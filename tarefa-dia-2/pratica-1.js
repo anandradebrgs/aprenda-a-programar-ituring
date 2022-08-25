32fb4101a6a9822c25e40e52f826cd37bf796d5f
ana <anandradeborges@hotmail.com>
2022-08-25 15:57:56 -0300

    msg-agencia-viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,7 +3,7 @@ let email = "anandradebgs@hotmail.com"
 let profissao = "empreendedora"
 let idade = "31"
 let viagem = "grecia"
-
-let mensagem = "ola, "+ nome +" Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ viagem + ". Confirma pra gente alguns dados? A sua idade é "+ idade + ", você é "+ profissao +" e o seu email é " + email" ? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
-
+let mensagem = "ola," + nome +" Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ viagem +" . Confirma pra gente alguns dados? A sua idade é "+idade +" você é "+profissao +" e o seu email é "+email +" ? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
 console.log(mensagem)
+
+
